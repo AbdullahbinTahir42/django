b8667cbe6761a34ef9f438bcb7780ab6c5a8a107
AbdullahbinTahir42 <abdullahbintahir42@gmail.com>
2025-02-04 14:53:33 +0500

    RestAPIs

--- FirstPro/FirstPro/settings.py
+++ FirstPro/FirstPro/settings.py
@@ -28,6 +28,9 @@ INSTALLED_APPS = [
 
 
     'base.apps.BaseConfig',
+    
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -38,6 +41,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'FirstPro.urls'
@@ -118,3 +123,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True 
